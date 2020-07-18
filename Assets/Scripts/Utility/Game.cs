d689176a7e282049f9e4db267a6c6a9d9c79a7fd
Gal Afik <gal.p.afik@gmail.com>
2020-07-18 15:52:35 -0400

    Added the persistent class and moved dev controls into a master Game class. Organized sprite folder and scene hierarchy view.

---
+++ Assets/Scripts/Utility/Game.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+	// Dev cheats control
+	public bool DevControls = false;
+
+	// Service objects
+
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (DevControls)
+		{
+			if (Input.GetKey(KeyCode.LeftShift))
+			{
+				// Speed up time
+				Time.timeScale = 2f;
+			}
+			else
+			{
+				// Set timescale back to normal
+				Time.timeScale = 1f;
+			}
+
+			if (Input.GetKeyDown(KeyCode.Escape))
+			{
+				// Quit the game
+				Application.Quit();
+			}
+
+			if (Input.GetKeyDown(KeyCode.R))
+			{
+				// Reload the current scene
+				SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Utility/Persistent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Persistent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		// Keep this object around after scene transitions
+		DontDestroyOnLoad(gameObject);
+    }
+}
